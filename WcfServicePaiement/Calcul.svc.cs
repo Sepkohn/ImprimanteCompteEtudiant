f164abbffdada8c8bd88e1ad2699fb68d13fa73e
Matth_HES <Matth_HES@users.noreply.github.com>
2020-05-17 14:19:30 +0200

    rename service calcul

--- WcfServicePaiement/Calcul.svc.cs
+++ WcfServicePaiement/Calcul.svc.cs
@@ -9,7 +9,7 @@ namespace WcfServicePaiement
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service2" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service2.svc or Service2.svc.cs at the Solution Explorer and start debugging.
-    public class Service2 : IService2
+    public class Service2 : ICalcul
     {
         public double Add(double a, double b)
         {
--- WcfServicePaiement/ICalcul.cs
+++ WcfServicePaiement/ICalcul.cs
@@ -9,7 +9,7 @@ namespace WcfServicePaiement
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService2" in both code and config file together.
     [ServiceContract]
-    public interface IService2
+    public interface ICalcul
     {
         [OperationContract]
         double Add(double a, double b);
