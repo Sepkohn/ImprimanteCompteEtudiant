4e23b41f3fb87c76767f3df647fdfb8ed9a2289f
Matth_HES <Matth_HES@users.noreply.github.com>
2020-05-29 17:36:30 +0200

    commit

--- WindowsFormsServicePaiement/Form1.Designer.cs
+++ WindowsFormsServicePaiement/Form1.Designer.cs
@@ -29,37 +29,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.etudiant = new System.Windows.Forms.Label();
+            this.solde = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.buttonPlus = new System.Windows.Forms.Button();
             this.buttonMoins = new System.Windows.Forms.Button();
-            this.buttonFois = new System.Windows.Forms.Button();
-            this.buttonDiv = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(133, 51);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Nombre 1";
+            this.etudiant.AutoSize = true;
+            this.etudiant.Location = new System.Drawing.Point(54, 39);
+            this.etudiant.Name = "label1";
+            this.etudiant.Size = new System.Drawing.Size(46, 13);
+            this.etudiant.TabIndex = 0;
+            this.etudiant.Text = "Etudiant";
+            this.etudiant.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(133, 177);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 13);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Nombre 2";
+            this.solde.AutoSize = true;
+            this.solde.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.solde.Location = new System.Drawing.Point(38, 180);
+            this.solde.Name = "label2";
+            this.solde.Size = new System.Drawing.Size(108, 20);
+            this.solde.TabIndex = 0;
+            this.solde.Text = "Solde (CHF)";
+            this.solde.Click += new System.EventHandler(this.label2_Click);
             // 
             // label3
             // 
@@ -71,127 +80,181 @@
             // 
             // buttonPlus
             // 
-            this.buttonPlus.Location = new System.Drawing.Point(149, 109);
+            this.buttonPlus.Location = new System.Drawing.Point(593, 177);
             this.buttonPlus.Name = "buttonPlus";
             this.buttonPlus.Size = new System.Drawing.Size(75, 23);
             this.buttonPlus.TabIndex = 2;
             this.buttonPlus.Text = "+";
             this.buttonPlus.UseVisualStyleBackColor = true;
             this.buttonPlus.Click += new System.EventHandler(this.buttonPlus_Click);
-
             // 
             // buttonMoins
             // 
-            this.buttonMoins.Location = new System.Drawing.Point(262, 109);
+            this.buttonMoins.Location = new System.Drawing.Point(574, 277);
             this.buttonMoins.Name = "buttonMoins";
             this.buttonMoins.Size = new System.Drawing.Size(75, 23);
             this.buttonMoins.TabIndex = 2;
-            this.buttonMoins.Text = "-";
+            this.buttonMoins.Text = "Print";
             this.buttonMoins.UseVisualStyleBackColor = true;
-            this.buttonMoins.Click += new System.EventHandler(this.buttonMoins_Click);
-
-            // 
-            // buttonFois
-            // 
-            this.buttonFois.Location = new System.Drawing.Point(378, 109);
-            this.buttonFois.Name = "buttonFois";
-            this.buttonFois.Size = new System.Drawing.Size(75, 23);
-            this.buttonFois.TabIndex = 2;
-            this.buttonFois.Text = "*";
-            this.buttonFois.UseVisualStyleBackColor = true;
-            this.buttonFois.Click += new System.EventHandler(this.buttonFois_Click);
-
-            // 
-            // buttonDiv
-            // 
-            this.buttonDiv.Location = new System.Drawing.Point(481, 109);
-            this.buttonDiv.Name = "buttonDiv";
-            this.buttonDiv.Size = new System.Drawing.Size(75, 23);
-            this.buttonDiv.TabIndex = 2;
-            this.buttonDiv.Text = "/";
-            this.buttonDiv.UseVisualStyleBackColor = true;
-            this.buttonDiv.Click += new System.EventHandler(this.buttonDiv_Click);
-
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(115, 283);
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(38, 276);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.Size = new System.Drawing.Size(173, 24);
             this.label4.TabIndex = 0;
-            this.label4.Text = "Result";
+            this.label4.Text = "Nombre de copie";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(291, 44);
+            this.textBox1.Location = new System.Drawing.Point(57, 55);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(218, 20);
             this.textBox1.TabIndex = 1;
-            //            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(291, 170);
+            this.textBox2.Location = new System.Drawing.Point(443, 177);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 1;
             // 
             // textBoxResult
             // 
-            this.textBoxResult.Location = new System.Drawing.Point(291, 280);
+            this.textBoxResult.Location = new System.Drawing.Point(443, 279);
             this.textBoxResult.Name = "textBoxResult";
             this.textBoxResult.Size = new System.Drawing.Size(100, 20);
             this.textBoxResult.TabIndex = 1;
-            //           this.textBoxResult.TextChanged += new System.EventHandler(this.textBoxResult_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(354, 39);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "ID Etudiant";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(348, 55);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(160, 20);
+            this.textBox3.TabIndex = 15;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(336, 226);
+            this.label5.Location = new System.Drawing.Point(270, 263);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(13, 13);
-            this.label5.TabIndex = 12;
-            this.label5.Text = "=";
+            this.label5.Size = new System.Drawing.Size(106, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Copie(s) disponible(s)";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(462, 263);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(66, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Copie à faire";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(590, 39);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(46, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "ID Carte";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(593, 55);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(160, 20);
+            this.textBox4.TabIndex = 19;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(273, 279);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 20;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(267, 156);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(84, 13);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "Solde disponible";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(444, 156);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(84, 13);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "Ajouter du crédit";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.textBoxResult);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.buttonDiv);
-            this.Controls.Add(this.buttonFois);
             this.Controls.Add(this.buttonMoins);
             this.Controls.Add(this.buttonPlus);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.solde);
+            this.Controls.Add(this.etudiant);
             this.Name = "Form1";
             this.Text = "Form1";
-            //            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label etudiant;
+        private System.Windows.Forms.Label solde;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button buttonPlus;
         private System.Windows.Forms.Button buttonMoins;
-        private System.Windows.Forms.Button buttonFois;
-        private System.Windows.Forms.Button buttonDiv;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
     }
 }
 
--- WindowsFormsServicePaiement/Form1.cs
+++ WindowsFormsServicePaiement/Form1.cs
@@ -16,5 +16,40 @@ namespace WindowsFormsServicePaiement
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonPlus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
